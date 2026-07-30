6cf97443a65c0ba169e09737f75eea406f7e0748
d6046540-pixel <d6046540@gmail.com>
2026-07-30 19:40:32 +0800

    Add LangChain prompt template

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from langchain_openai import ChatOpenAI
-from config import SILICONFLOW_API_KEY, BASE_URL
+from langchain_core.prompts import ChatPromptTemplate
 
+from config import SILICONFLOW_API_KEY, BASE_URL
 
 llm = ChatOpenAI(
     api_key=SILICONFLOW_API_KEY,
@@ -9,8 +10,19 @@ llm = ChatOpenAI(
     temperature=0
 )
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "你是一位专业的Python导师，请用简洁、易懂的方式回答问题。"),
+        ("human", "{question}")
+    ]
+)
 
-response = llm.invoke("你好，请简单介绍一下你自己。")
+chain = prompt | llm
 
+response = chain.invoke(
+    {
+        "question": "什么是LangChain？"
+    }
+)
 
 print(response.content)
